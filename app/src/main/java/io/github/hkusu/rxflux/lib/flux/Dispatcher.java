089a818d9593409138adab276d23522c78b62ebc
hkusu <kusu0806@gmail.com>
2016-11-02 12:14:02 +0900

    Change the interface of the flux library

--- app/src/main/java/io/github/hkusu/rxflux/lib/flux/Dispatcher.java
+++ app/src/main/java/io/github/hkusu/rxflux/lib/flux/Dispatcher.java
@@ -2,22 +2,27 @@ package io.github.hkusu.rxflux.lib.flux;
 
 import android.support.annotation.NonNull;
 
+import java.util.Objects;
+
 import rx.Observable;
+import rx.functions.Action1;
 import rx.subjects.PublishSubject;
 import rx.subjects.SerializedSubject;
 import rx.subjects.Subject;
 
 public final class Dispatcher {
-    private final Subject<Action, Action> subject = new SerializedSubject<>(PublishSubject.create());
+    private final Subject<Action, Action> dispatcherSubject = new SerializedSubject<>(PublishSubject.create());
 
     public Dispatcher() {
     }
 
-    final Observable<Action> observable() {
-        return subject;
+    final Observable<Action> on(@NonNull Action.Key key, @NonNull Action1<Action> rxAction) {
+        return dispatcherSubject
+                .filter(fluxAction -> Objects.equals(fluxAction.key, key))
+                .doOnNext(rxAction);
     }
 
-    final void dispatch(@NonNull Action action) {
-        subject.onNext(action);
+    final void dispatch(@NonNull Action fluxAction) {
+        dispatcherSubject.onNext(fluxAction);
     }
 }
--- app/src/main/java/io/github/hkusu/rxflux/lib/flux/Store.java
+++ app/src/main/java/io/github/hkusu/rxflux/lib/flux/Store.java
@@ -3,8 +3,6 @@ package io.github.hkusu.rxflux.lib.flux;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
-import java.util.Objects;
-
 import rx.Scheduler;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.functions.Action1;
@@ -52,9 +50,7 @@ public abstract class Store implements Action.Key {
     // また現状ではStoreはシングルトンでActivityが破棄されても生存し続け再利用される
     // (辞めたい場合はDaggerの@Singletonアノテーションを外しCompositeSubscriptionで管理)
     protected final void on(@NonNull Action.Key key, @NonNull Action1<Action> rxAction) {
-        this.dispatcher.observable()
-                .filter(fluxAction -> Objects.equals(fluxAction.key, key))
-                .doOnNext(rxAction)
+        dispatcher.on(key, rxAction)
                 .subscribeOn(Schedulers.io())
                 .subscribe(fluxAction -> {
                     if (fluxAction.notifyStoreChanged) {
